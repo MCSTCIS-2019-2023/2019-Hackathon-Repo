a08aa12d78d2c20889cc11a85ad6b08cfbe323c7
TeslaSTech <55847611+TeslaSTech@users.noreply.github.com>
2019-11-09 16:12:30 -0500

    fortnite bad

--- hack.js
+++ hack.js
@@ -1,6 +1,6 @@
 // PS! Replace this with your own channel ID
 // If you use this channel ID your app will stop working in the future
-const CLIENT_ID = 'EduScaleChat1';
+const CLIENT_ID = 'OkECGUsOKPqj4oaK';
 
 const drone = new ScaleDrone(CLIENT_ID, {
     data: { // Will be sent out as clientData via events
